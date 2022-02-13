a89d1c9c4c490455947618cd42baec4eeeb6c5a2
Soumen <soumenkp2@gmail.com>
2022-02-13 09:21:07 +0530

    Added home

---
+++ app/src/main/java/com/example/bhuk/SocioMoney.java
@@ -0,0 +1,14 @@
+package com.example.bhuk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SocioMoney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_socio_money);
+    }
+}
--- app/src/main/java/com/example/bhuk/home.java
+++ app/src/main/java/com/example/bhuk/home.java
@@ -1,5 +1,6 @@
 package com.example.bhuk;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +18,9 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class home extends Fragment {
+    TextView NGO, orphanage, oldage, individual;
+    GridLayout grid1, grid2, grid3, grid4, grid5, grid6, grid7;
+    LinearLayout sociomoney;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +66,105 @@ public class home extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        NGO = (TextView) view.findViewById(R.id.NGO);
+        orphanage = (TextView) view.findViewById(R.id.orphanages);
+        oldage = (TextView) view.findViewById(R.id.old_age_home);
+        individual = (TextView) view.findViewById(R.id.individuals);
+        sociomoney = (LinearLayout) view.findViewById(R.id.socio_money);
+
+        grid1 = (GridLayout) view.findViewById(R.id.grid1);
+        grid2 = (GridLayout) view.findViewById(R.id.grid2);
+        grid3 = (GridLayout) view.findViewById(R.id.grid3);
+        grid4 = (GridLayout) view.findViewById(R.id.grid4);
+        grid5 = (GridLayout) view.findViewById(R.id.grid5);
+        grid6 = (GridLayout) view.findViewById(R.id.grid6);
+        grid7 = (GridLayout) view.findViewById(R.id.grid7);
+
+        grid1.setVisibility(View.VISIBLE);
+        grid2.setVisibility(View.VISIBLE);
+        grid3.setVisibility(View.GONE);
+        grid4.setVisibility(View.GONE);
+        grid5.setVisibility(View.GONE);
+        grid6.setVisibility(View.GONE);
+        grid7.setVisibility(View.GONE);
+
+        NGO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NGO.setBackgroundResource(R.drawable.bluebg);
+                orphanage.setBackgroundResource(R.drawable.whitebg);
+                oldage.setBackgroundResource(R.drawable.whitebg);
+                individual.setBackgroundResource(R.drawable.whitebg);
+                grid1.setVisibility(View.VISIBLE);
+                grid2.setVisibility(View.VISIBLE);
+                grid3.setVisibility(View.GONE);
+                grid4.setVisibility(View.GONE);
+                grid5.setVisibility(View.GONE);
+                grid6.setVisibility(View.GONE);
+                grid7.setVisibility(View.GONE);
+            }
+        });
+
+        orphanage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                orphanage.setBackgroundResource(R.drawable.bluebg);
+                NGO.setBackgroundResource(R.drawable.whitebg);
+                oldage.setBackgroundResource(R.drawable.whitebg);
+                individual.setBackgroundResource(R.drawable.whitebg);
+                grid1.setVisibility(View.GONE);
+                grid2.setVisibility(View.GONE);
+                grid3.setVisibility(View.VISIBLE);
+                grid4.setVisibility(View.VISIBLE);
+                grid5.setVisibility(View.GONE);
+                grid6.setVisibility(View.GONE);
+                grid7.setVisibility(View.GONE);
+            }
+        });
+
+        oldage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                oldage.setBackgroundResource(R.drawable.bluebg);
+                orphanage.setBackgroundResource(R.drawable.whitebg);
+                NGO.setBackgroundResource(R.drawable.whitebg);
+                individual.setBackgroundResource(R.drawable.whitebg);
+                grid1.setVisibility(View.GONE);
+                grid2.setVisibility(View.GONE);
+                grid3.setVisibility(View.GONE);
+                grid4.setVisibility(View.GONE);
+                grid5.setVisibility(View.VISIBLE);
+                grid6.setVisibility(View.VISIBLE);
+                grid7.setVisibility(View.GONE);
+            }
+        });
+
+        individual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                individual.setBackgroundResource(R.drawable.bluebg);
+                orphanage.setBackgroundResource(R.drawable.whitebg);
+                NGO.setBackgroundResource(R.drawable.whitebg);
+                oldage.setBackgroundResource(R.drawable.whitebg);
+                grid1.setVisibility(View.GONE);
+                grid2.setVisibility(View.GONE);
+                grid3.setVisibility(View.GONE);
+                grid4.setVisibility(View.GONE);
+                grid5.setVisibility(View.GONE);
+                grid6.setVisibility(View.GONE);
+                grid7.setVisibility(View.VISIBLE);
+            }
+        });
+
+        sociomoney.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getContext(),SocioMoney.class);
+                startActivity(i);
+            }
+        });
+        return view;
     }
 }
