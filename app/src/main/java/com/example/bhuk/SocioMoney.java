ed434953406c59521eb4e98f07216fe838decb9d
Soumen <soumenkp2@gmail.com>
2022-02-13 09:25:57 +0530

    socio money

--- app/src/main/java/com/example/bhuk/SocioMoney.java
+++ app/src/main/java/com/example/bhuk/SocioMoney.java
@@ -2,13 +2,26 @@ package com.example.bhuk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SocioMoney extends AppCompatActivity {
+    ImageView backbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_socio_money);
+
+        backbutton = findViewById(R.id.backbutton);
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SocioMoney.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
